19f375969bb1d6f662377e528cead3ae24184bd6
Anesu <anesu4@yahoo.com.au>
2020-10-22 11:53:22 +1100

    Build Development Branch

---
+++ mp_ss_debt_collection.js
@@ -0,0 +1,39 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ScheduledScript
+ * 
+ * Module Description
+ * 
+ * NSVersion    Date                        Author         
+ * 2.00         2020-10-22 09:33:08         Anesu
+ *
+ * Description: Automation of Debt Collection Process   
+ * 
+ * @Last Modified by:   Anesu
+ * @Last Modified time: 2020-10-22 16:49:26
+ * 
+ */
+
+define(['N/ui/serverWidget', 'N/email', 'N/runtime', 'N/search', 'N/record', 'N/http', 'N/log', 'N/redirect'],
+    function(ui, email, runtime, search, record, http, log, redirect) {
+        var zee = 0;
+        var role = 0;
+
+        var baseURL = 'https://system.na2.netsuite.com';
+        if (runtime.EnvType == "SANDBOX") {
+            baseURL = 'https://system.sandbox.netsuite.com';
+        }
+
+        role = runtime.getCurrentUser().role;
+
+        if (role == 1000) {
+            zee = runtime.getCurrentUser().id;
+        } else if (role == 3) { //Administrator
+            zee = 6; //test
+        } else if (role == 1032) { // System Support
+            zee = 425904; //test-AR
+        }
+
+        
+    }
+);
